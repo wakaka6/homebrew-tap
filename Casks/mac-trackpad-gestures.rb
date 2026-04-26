edb25d31c07f27bad07c67d78644d48997006fb7
wakaka6 <wakaka6@users.noreply.github.com>
2026-04-27 02:46:10 +0800

    Update MacGesture cask checksum

--- Casks/mac-trackpad-gestures.rb
+++ Casks/mac-trackpad-gestures.rb
@@ -1,6 +1,6 @@
 cask "mac-trackpad-gestures" do
   version "4.1.0"
-  sha256 "fcdfefd8788a3c48540d0a477d6b43b28022a977c111d35b3c7c73bb60276355"
+  sha256 "6ebadc3b81f428b0d559dcb0076006526f21e3d362feb21f26b4314be2f8c802"
 
   url "https://github.com/wakaka6/mac-trackpad-gestures/releases/download/v#{version}/MacGesture-#{version}.dmg"
   name "MacGesture"
